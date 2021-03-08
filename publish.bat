ba7b8978a7dc92eb6b5c5ee31568fc7195f29cd7
Shadowsun™ <FarukGamer@users.noreply.github.com>
2021-03-08 21:25:54 +0100

    Create upload script

---
+++ publish.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set "basepath=D:\SteamLibrary\steamapps\common\GarrysMod\bin\win64"
+set "gmad=%basepath%\gmad.exe"
+set "gmpublish=%basepath%\gmpublish.exe"
+
+set "publish_path=workshop"
+set "publish_gma=workshop.gma"
+set "publish_id=2419005587"
+
+call %gmad% create -folder %publish_path%
+call %gmpublish% update -addon %publish_gma% -id %publish_id%
+del %publish_gma%
+
+pause
