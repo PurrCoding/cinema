dcefeeec483a62f3aac597b54e0bc0c02d37b978
Shadowsun™ <FarukGamer@users.noreply.github.com>
2021-03-21 23:09:48 +0100

    Create cl_interface.lua

---
+++ workshop/gamemodes/cinema/gamemode/modules/theater/cl_interface.lua
@@ -0,0 +1,46 @@
+local THEATER_INTERFACE = [[
+    if (!window.theater) {
+        class TheaterController {
+
+            get player() {
+                return window.player;
+            }
+
+            setVolume(volume) {
+                if (!!this.player) {
+                    this.player.volume = volume / 100;
+                }
+            }
+
+            seek(second) {
+                if (!!this.player && !!this.player.currentTime) {
+                    this.player.currentTime = second;
+                }
+            }
+
+            sync(time) {
+                if (!!this.player && !!this.player.currentTime && !!time) {
+
+                    var current = this.player.currentTime;
+                    if ((current !== null) &&
+                        (Math.abs(time - current) > 5)) {
+                        this.player.currentTime = time;
+                    }
+                }
+            }
+
+            enableHD(on) { }
+        };
+        window.theater = new TheaterController();
+    }
+]]
+
+module( "theater", package.seeall )
+
+function startController()
+
+    local panel = ActivePanel()
+    if IsValid(panel) then
+        panel:QueueJavascript(THEATER_INTERFACE)
+    end
+end
