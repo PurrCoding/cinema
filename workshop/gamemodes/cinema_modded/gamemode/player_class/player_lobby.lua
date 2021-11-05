5c1592d012c7a5740d045c933aafd560fb433704
Shadowsun™ <FarukGamer@users.noreply.github.com>
2021-11-05 20:46:02 +0100

    fix slow walking

--- workshop/gamemodes/cinema_modded/gamemode/player_class/player_lobby.lua
+++ workshop/gamemodes/cinema_modded/gamemode/player_class/player_lobby.lua
@@ -15,6 +15,7 @@ PLAYER.TauntCam = TauntCamera()
 PLAYER.DisplayName			= "Lobby Class"
 
 PLAYER.WalkSpeed 			= 200		-- How fast to move when not running
+PLAYER.SlowWalkSpeed		= 100		-- How fast to move when slow-walking (+WALK)
 PLAYER.RunSpeed				= 300		-- How fast to move when running
 PLAYER.CrouchedWalkSpeed 	= 0.2		-- Multiply move speed by this when crouching
 PLAYER.DuckSpeed			= 0.3		-- How fast to go from not ducking, to ducking
