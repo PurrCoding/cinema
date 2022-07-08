96ba5601faaff7328ed67d476e6bb77b9441d46f
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-07-08 18:31:26 +0200

    Update index.js

--- public/functions/cors/index.js
+++ public/functions/cors/index.js
@@ -1,5 +1,4 @@
-export async function onRequest(context) {
-	const { request } = context;
+export async function onRequestGet(request) {
 
 	isOPTIONS = (request.method == "OPTIONS");
 	var origin_url = new URL(request.url);
