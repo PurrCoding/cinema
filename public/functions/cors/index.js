0785e88a9d33d6207f7a8bb95ce9a64b2dedc709
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-07-08 18:15:57 +0200

    Update index.js

--- public/functions/cors/index.js
+++ public/functions/cors/index.js
@@ -2,6 +2,8 @@ export async function onRequest(context) {
 	const { request, env } = context;
 	const { pathname, searchParams } = new URL(request.url);
 
-	// Respond to the requesting caller with the response of the proxied resource.
-	return env.ASSETS.fetch(searchParams.get("url"));
+
+	fetch(searchParams.get("url")); {
+		return new Response('Hello');
+	}
 }
