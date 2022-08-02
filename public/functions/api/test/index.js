81ad50b6775e8ada850db21b9ad3d6bb773944b3
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-08-02 21:40:54 +0200

    Update index.js

--- public/functions/api/test/index.js
+++ public/functions/api/test/index.js
@@ -9,5 +9,5 @@ export async function onRequest(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
   
-    return new Response(request.hostname);
+    return new Response("mew");
   }
