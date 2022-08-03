0556259081ac4f9c3591cf23ab9feb3b45fe53e6
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-08-03 12:09:21 +0200

    Update sh_youtube.lua

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_youtube.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_youtube.lua
@@ -29,65 +29,27 @@ if (CLIENT) then
 	end
 end
 
----
--- Get the value for an attribute from a html element
---
-local function ParseElementAttribute( element, attribute )
-	if not element then return end
-	-- Find the desired attribute
-	local output = string.match( element, attribute .. "%s-=%s-%b\"\"" )
-	if not output then return end
-	-- Remove the 'attribute=' part
-	output = string.gsub( output, attribute .. "%s-=%s-", "" )
-	-- Trim the quotes around the value string
-	return string.sub( output, 2, -2 )
-end
-
----
--- Get the contents of a html element by removing tags
--- Used as fallback for when title cannot be found
---
-local function ParseElementContent( element )
-	if not element then return end
-	-- Trim start
-	local output = string.gsub( element, "^%s-<%w->%s-", "" )
-	-- Trim end
-	return string.gsub( output, "%s-</%w->%s-$", "" )
-end
-
--- Lua search patterns to find metadata from the html
-local patterns = {
-	["title"] = "<meta%sproperty=\"og:title\"%s-content=%b\"\">",
-	["title_fallback"] = "<title>.-</title>",
-	["thumb"] = "<meta%sproperty=\"og:image\"%s-content=%b\"\">",
-	["thumb_fallback"] = "<link%sitemprop=\"thumbnailUrl\"%s-href=%b\"\">",
-	["duration"] = "<meta%sitemprop%s-=%s-\"duration\"%s-content%s-=%s-%b\"\">",
-	["live"] = "<meta%sitemprop%s-=%s-\"isLiveBroadcast\"%s-content%s-=%s-%b\"\">",
-	["live_enddate"] = "<meta%sitemprop%s-=%s-\"endDate\"%s-content%s-=%s-%b\"\">",
-	["age_restriction"] = "<meta%sproperty=\"og:restrictions:age\"%s-content=%b\"\">"
-}
-
 function SERVICE:GetURLInfo( url )
 
 	local info = {}
 
 	-- http://www.youtube.com/watch?v=(videoId)
-	if url.query and url.query.v and string.len(url.query.v) > 0 then
+	if url.query and url.query.v and #url.query.v > 0 then
 		info.Data = url.query.v
 
 	-- http://www.youtube.com/v/(videoId)
-	elseif url.path and string.match(url.path, "^/v/([%a%d-_]+)") then
-		info.Data = string.match(url.path, "^/v/([%a%d-_]+)")
+	elseif url.path and url.path:match("^/v/([%a%d-_]+)") then
+		info.Data = url.path:match("^/v/([%a%d-_]+)")
 
 		-- http://www.youtube.com/shorts/(videoId)
-	elseif url.path and string.match(url.path, "^/shorts/([%a%d-_]+)") then
-		info.Data = string.match(url.path, "^/shorts/([%a%d-_]+)")
+	elseif url.path and url.path:match("^/shorts/([%a%d-_]+)") then
+		info.Data = url.path:match("^/shorts/([%a%d-_]+)")
 
 	-- http://youtu.be/(videoId)
-	elseif string.match(url.host, "youtu.be") and
-		url.path and string.match(url.path, "^/([%a%d-_]+)$") and
+	elseif url.host:match("youtu.be") and
+		url.path and url.path:match("^/([%a%d-_]+)$") and
 		( not info.query or #info.query == 0 ) then -- short url
-		info.Data = string.match(url.path, "^/([%a%d-_]+)$")
+		info.Data = url.path:match("^/([%a%d-_]+)$")
 	end
 
 	-- Start time, ?t=123s
@@ -106,36 +68,48 @@ function SERVICE:GetURLInfo( url )
 
 end
 
+-- Lua search patterns to find metadata from the html
+local patterns = {
+	["title"] = "<meta%sproperty=\"og:title\"%s-content=%b\"\">",
+	["title_fallback"] = "<title>.-</title>",
+	["thumb"] = "<meta%sproperty=\"og:image\"%s-content=%b\"\">",
+	["thumb_fallback"] = "<link%sitemprop=\"thumbnailUrl\"%s-href=%b\"\">",
+	["duration"] = "<meta%sitemprop%s-=%s-\"duration\"%s-content%s-=%s-%b\"\">",
+	["live"] = "<meta%sitemprop%s-=%s-\"isLiveBroadcast\"%s-content%s-=%s-%b\"\">",
+	["live_enddate"] = "<meta%sitemprop%s-=%s-\"endDate\"%s-content%s-=%s-%b\"\">",
+	["age_restriction"] = "<meta%sproperty=\"og:restrictions:age\"%s-content=%b\"\">"
+}
+
 ---
 -- Function to parse video metadata straight from the html instead of using the API
 --
-function SERVICE:ParseYTMetaDataFromHTML( html )
+local function ParseMetaDataFromHTML( html )
 	--MetaData table to return when we're done
 	local metadata = {}
 
 	-- Fetch title and thumbnail, with fallbacks if needed
-	metadata.title = ParseElementAttribute(string.match(html, patterns["title"]), "content")
-		or ParseElementContent(string.match(html, patterns["title_fallback"]))
+	metadata.title = util.ParseElementAttribute(html:match(patterns["title"]), "content")
+		or util.ParseElementContent(html:match(patterns["title_fallback"]))
 
 	-- Parse HTML entities in the title into symbols
 	metadata.title = url.htmlentities_decode(metadata.title)
 
-	metadata.thumbnail = ParseElementAttribute(string.match(html, patterns["thumb"]), "content")
-		or ParseElementAttribute(string.match(html, patterns["thumb_fallback"]), "href")
+	metadata.thumbnail = util.ParseElementAttribute(html:match(patterns["thumb"]), "content")
+		or util.ParseElementAttribute(html:match(patterns["thumb_fallback"]), "href")
 
-	metadata.familyfriendly = ParseElementAttribute(string.match(html, patterns["age_restriction"]), "content") or ""
+	metadata.familyfriendly = util.ParseElementAttribute(html:match(patterns["age_restriction"]), "content") or ""
 
 	-- See if the video is an ongoing live broadcast
 	-- Set duration to 0 if it is, otherwise use the actual duration
-	local isLiveBroadcast = tobool(ParseElementAttribute(string.match(html, patterns["live"]), "content"))
-	local broadcastEndDate = string.match(html, patterns["live_enddate"])
+	local isLiveBroadcast = tobool(util.ParseElementAttribute(html:match(patterns["live"]), "content"))
+	local broadcastEndDate = html:match(patterns["live_enddate"])
 	if isLiveBroadcast and not broadcastEndDate then
 		-- Mark as live video
 		metadata.duration = 0
 	else
-		local durationISO8601 = ParseElementAttribute(string.match(html, patterns["duration"]), "content")
+		local durationISO8601 = util.ParseElementAttribute(html:match(patterns["duration"]), "content")
 		if isstring(durationISO8601) then
-			metadata.duration = math.max(1, convertISO8601Time(durationISO8601))
+			metadata.duration = math.max(1, util.ISO_8601ToSeconds(durationISO8601))
 		end
 	end
 
@@ -145,7 +119,7 @@ end
 function SERVICE:GetVideoInfo( data, onSuccess, onFailure )
 
 	local onReceive = function( body, length, headers, code )
-		local status, metadata = pcall(self.ParseYTMetaDataFromHTML, self, body)
+		local status, metadata = pcall(ParseMetaDataFromHTML, body)
 		if not status  then
 			return onFailure( "Theater_RequestFailed" )
 		end
