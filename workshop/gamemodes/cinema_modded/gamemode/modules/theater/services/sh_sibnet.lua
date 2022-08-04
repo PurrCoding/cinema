70dbc10c6cbeac1da5d4a11c4eb466389de2540e
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-08-04 20:23:57 +0200

    Make Sibnet Independent from Cloudflare Workers

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_sibnet.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_sibnet.lua
@@ -14,14 +14,14 @@ SERVICE.IsTimed = true
 ]]--
 -- SERVICE.TheaterType = THEATER_PRIVATE
 
-local API_URL = "https://gmod-cinema.pages.dev/api/sibnet?v=%s"
+local API_URL = "https://video.sibnet.ru/shell.php?videoid=%s"
 
 function SERVICE:Match( url )
 	return url.host and url.host:match("video.sibnet.ru")
 end
 
 if (CLIENT) then
-	local EMBED_URL = "https://video.sibnet.ru/shell.php?videoid=%s" 
+	local EMBED_URL = "https://video.sibnet.ru/shell.php?videoid=%s"
 	local THEATER_JS = [[
 		function check() {
 			var preplayer = document.getElementById("video_html5_wrapper_html5_api")
@@ -77,25 +77,42 @@ function SERVICE:GetURLInfo( url )
 	return false
 end
 
+-- Lua search patterns to find metadata from the html
+local patterns = {
+	["title"] = "<meta%sproperty=\"og:title\"%s-content=%b\"\"/>",
+	["thumb"] = "<meta%sproperty=\"og:image\"%s-content=%b\"\"/>",
+	["duration"] = "<meta%sproperty=\"og:duration\"%s-content=%b\"\"[%s]/>"
+}
+
+---
+-- Function to parse video metadata straight from the html instead of using the API
+--
+local function ParseMetaDataFromHTML( html )
+	local metadata, html = {}, html
+
+	metadata.title = util.ParseElementAttribute(html:match(patterns["title"]), "content")
+	metadata.title = url.htmlentities_decode(metadata.title) -- Parse HTML entities in the title into symbols
+	metadata.title = util.win_to_utf8(metadata.title, 1251) -- Convert Windows-1251 Charset to UTF-8
+
+	metadata.thumbnail = util.ParseElementAttribute(html:match(patterns["thumb"]), "content")
+	metadata.duration = tonumber(util.ParseElementAttribute(html:match(patterns["duration"]), "content"))
+
+	return metadata
+end
+
 function SERVICE:GetVideoInfo( data, onSuccess, onFailure )
 
 	local onReceive = function( body, length, headers, code )
 
-		local response = util.JSONToTable( body )
-		if not response then
-			return onFailure("Sibnet: No response from API")
+		local status, metadata = pcall(ParseMetaDataFromHTML, body)
+		if not status  then
+			return onFailure( "Theater_RequestFailed" )
 		end
 
 		local info = {}
-		info.title = ("Sibnet: %s"):format(data)
-		info.thumbnail = response.thumbnail
-		if response.duration == "0" then
-			return onFailure("Sibnet: Livestream currently not supported")
-		else
-			info.duration = response.duration
-		end
-
-		info.data = response.embed
+		info.title = metadata.title
+		info.thumbnail = metadata.thumbnail
+		info.duration = metadata.duration
 
 		if onSuccess then
 			pcall(onSuccess, info)
@@ -104,7 +121,10 @@ function SERVICE:GetVideoInfo( data, onSuccess, onFailure )
 	end
 
 	local url = API_URL:format(data)
-	self:Fetch( url, onReceive, onFailure )
+	self:Fetch( url, onReceive, onFailure, {
+		["Accept-Encoding"] = "identity;q=1, *;q=0",
+		["Accept-Language"] = "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5",
+	} )
 
 end
 
