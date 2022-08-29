aca6519450d6c17ce7f68c69fa595577a843c2d7
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-08-29 22:43:22 +0200

    Update sh_sbox.lua

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/sh_sbox.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/sh_sbox.lua
@@ -1,15 +1,10 @@
 
 if SERVER then
-    hook.Add("PlayerLoadout", "CinemaSbox_Loadout", function(ply)
-        if (GAMEMODE.IsSandboxDerived and IsValid(ply)) then
-            ply:Give("gmod_tool")
-        end
-    end)
-
     hook.Add("PlayerSpawn", "CinemaSbox_Spawn", function(ply, transition)
-        if (GAMEMODE.IsSandboxDerived and IsValid(ply)) then
+        if (GAMEMODE.IsSandboxDerived and IsValid( ply )) then
             timer.Simple(0, function()
                 player_manager.SetPlayerClass( ply, "player_sandbox" )
+                player_manager.RunClass( ply, "Loadout")
                 player_manager.OnPlayerSpawn( ply )
             end)
         end
