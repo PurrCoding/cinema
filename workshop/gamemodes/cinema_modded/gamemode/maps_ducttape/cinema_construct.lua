04dd8b17ed5584b03acf6556aa2210eb67aa3ad4
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-08-29 20:56:11 +0200

    Create cinema_construct.lua

---
+++ workshop/gamemodes/cinema_modded/gamemode/maps_ducttape/cinema_construct.lua
@@ -0,0 +1,4 @@
+-- Some changes to the cinema_construct map, because the author does not update the map anymore.
+
+-- Remove the hooks for adding prop_dynamic doors
+hook.Remove("InitPostEntity", "CinemaConstructInitGamemode")
