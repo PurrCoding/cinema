36d5caecad6940c3936a235bc645dc8ee60739b3
Shadowsun™ <FarukGamer@users.noreply.github.com>
2022-11-27 23:50:45 +0100

    Update credits.lua

---
+++ workshop/gamemodes/cinema_modded/gamemode/credits.lua
@@ -0,0 +1,37 @@
+local credit = [[
+	 ██████ ██ ███    ██ ███████ ███    ███  █████
+	██      ██ ████   ██ ██      ████  ████ ██   ██
+	██      ██ ██ ██  ██ █████   ██ ████ ██ ███████
+	██      ██ ██  ██ ██ ██      ██  ██  ██ ██   ██
+	 ██████ ██ ██   ████ ███████ ██      ██ ██   ██
+
+
+		███████ ██ ██   ██ ███████ ██████      ███████ ██████  ██ ████████ ██  ██████  ███    ██
+		██      ██  ██ ██  ██      ██   ██     ██      ██   ██ ██    ██    ██ ██    ██ ████   ██
+		█████   ██   ███   █████   ██   ██     █████   ██   ██ ██    ██    ██ ██    ██ ██ ██  ██
+		██      ██  ██ ██  ██      ██   ██     ██      ██   ██ ██    ██    ██ ██    ██ ██  ██ ██
+		██      ██ ██   ██ ███████ ██████      ███████ ██████  ██    ██    ██  ██████  ██   ████
+
+
+	This AddOn is released under the GNU GPLv3 license (https://choosealicense.com/licenses/gpl-3.0/) and
+	its Source Code available on GitHub (https://github.com/FarukGamer/cinema/)
+
+	Credits:
+	- Originally developed by PixelTail Games
+	- Shadowsun™ (STEAM_0:1:75888605 | https://bio.link/shadowsun) for maintaining the addon
+	- YouTube Workaround by Veitikka (https://github.com/veitikka)
+	- Ket'Ta-Lani (STEAM_0:0:63267722) & Artar0s (STEAM_0:0:85997797) for Sandbox in Cinema
+	{@TransInsert} 
+]]
+
+if CLIENT then
+	credit = credit:Replace("{@TransInsert}",
+		("- %s translation by %s"):format(
+			translations:Format("Name"),
+			translations:Format("Author")
+		))
+else
+	credit = credit:Replace("{@TransInsert}", "")
+end
+
+print(credit)
