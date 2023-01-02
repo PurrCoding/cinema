fc63cbfe07ebf817e42808ea0e5f6a7c47cdc145
Shadowsun™ <FarukGamer@users.noreply.github.com>
2023-01-02 18:26:12 +0100

    Update sh_file.lua

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_file.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_file.lua
@@ -45,7 +45,20 @@ if (CLIENT) then
 	local HTML_BASE = [[
 		<html>
 		<head></head>
-		<body style="margin:0px;background-color:black;overflow:hidden;">
+		<body>
+			<style>
+				body {
+					margin:0px;
+					background-color:black;
+					overflow:hidden;
+				}
+
+				video {
+					height: 100%;
+					width: 100%;
+				}
+			</style>
+
 			<div id="player-wrapper"></div>
 		
 			<script>
@@ -54,8 +67,6 @@ if (CLIENT) then
 				video.autoplay = true;
 				video.controls = true;
 				video.muted = false;
-				video.height = window.innerHeight;
-				video.width = window.innerWidth;
 				
 				document.getElementById("player-wrapper").appendChild(video);
 			</script>
