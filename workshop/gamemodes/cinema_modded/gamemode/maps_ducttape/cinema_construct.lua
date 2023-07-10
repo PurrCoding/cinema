0b556c69ff1b004bd12f0e197e7c740a56cf8b46
Shadowsun™ <FarukGamer@users.noreply.github.com>
2023-07-10 15:45:50 +0200

    Remove ducttape for construct

--- workshop/gamemodes/cinema_modded/gamemode/maps_ducttape/cinema_construct.lua
+++
@@ -1,4 +0,0 @@
--- Some changes to the cinema_construct map, because the author does not update the map anymore.
-
--- Remove the hooks for adding prop_dynamic doors
-hook.Remove("InitPostEntity", "CinemaConstructInitGamemode")
