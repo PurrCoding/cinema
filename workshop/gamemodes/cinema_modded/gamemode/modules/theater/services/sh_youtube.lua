948457a6a3ce3c79cdf465f5ccef4e11b6c0d8b1
Shadowsun™ <12494967+CattoGamer@users.noreply.github.com>
2024-08-06 14:19:15 +0200

    Invidious/YouTube requieres full dependecy

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_youtube.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_youtube.lua
@@ -3,7 +3,7 @@ local SERVICE = {}
 SERVICE.Name = "YouTube"
 SERVICE.IsTimed = true
 
-SERVICE.Dependency = DEPENDENCY_PARTIAL
+SERVICE.Dependency = DEPENDENCY_COMPLETE
 SERVICE.ExtentedVideoInfo = true
 
 local hostname = ""
