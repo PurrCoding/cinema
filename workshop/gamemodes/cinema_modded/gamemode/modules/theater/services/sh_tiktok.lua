6db244d2d651ad75d87079e7012d614652d48316
Shadowsun™ <12494967+CattoGamer@users.noreply.github.com>
2025-03-12 02:55:24 +0100

    Update sh_tiktok.lua

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_tiktok.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/services/sh_tiktok.lua
@@ -1,131 +1,188 @@
 local SERVICE = {}
+
 SERVICE.Name = "Tiktok"
 SERVICE.IsTimed = true
+
 SERVICE.Dependency = DEPENDENCY_COMPLETE
 SERVICE.ExtentedVideoInfo = true
 
+local EMBED_URL = "https://www.tiktok.com/embed/v3/%s"
+local EMBED_PARAM = "?controls=0&fullscreen_button=0&play_button=0&volume_control=0&timestamp=0&loop=0&description=0&music_info=0&rel=0"
+
 function SERVICE:Match(url)
-    return url.host and url.host:match("www.tiktok.com")
+	return url.host and url.host:match("tiktok.com")
 end
 
-local EMBED_URL = "https://www.tiktok.com/player/v1/%s?controls=0&autoplay=1&fullscreen_button=0&play_button=0&volume_control=0&timestamp=0&loop=0&description=0&music_info=0&rel=0"
-local API_URL = "https://www.tiktok.com/oembed?url=%s"
-
 if CLIENT then
-    local THEATER_JS = [[ 
-          
-        setInterval(function() { 
-            document.querySelector('video').setAttribute('controls', '');
-            document.querySelector('video').muted = false;
-            window.cinema_controller = document.querySelector('video');
-            exTheater.controllerReady();
-        }, 100);
+	local THEATER_JS = [[
+			var done = false
+			var checkerInterval = setInterval(function () {
 
-    ]]
-    function SERVICE:LoadProvider(Video, panel)
-        panel:OpenURL(EMBED_URL:format(Video:Data():match("/@[%a%w%d%_%.]+/video/(%d+)$")))
-        panel.OnDocumentReady = function(pnl)
-            SERVICE:LoadExFunctions(pnl, THEATER_INTERFACE)
-            pnl:QueueJavascript(THEATER_JS)
-        end
-    end
+				var player = document.getElementsByTagName("VIDEO")[0]
+				if (!!player) {
 
-    function SERVICE:GetMetadata(data, callback)
-        local panel = vgui.Create("DHTML")
-        panel:SetMouseInputEnabled(false)
-        panel.OnDocumentReady = function(pnl)
-            local METADATA_JS = [[
-                const convertToSeconds = time => {
-                    const [minutes, seconds] = time.split(':').map(Number);
-                    return (minutes * 60) + seconds;
-                };
-                
-                // Check and return the value when conditions are met
-                const checkSeekBarTime = () => {
-                    const seekBarTimeDiv = Array.from(document.querySelectorAll('div'))
-                        .find(div => /DivSeekBarTimeContainer/.test(div.className));
-                
-                    if (seekBarTimeDiv) {
-                        const [ , endTime ] = seekBarTimeDiv.textContent.split('/').map(s => s.trim());
-                        const totalSeconds = convertToSeconds(endTime);
-                
-                        if (totalSeconds > 1) {
-                            console.log("CINEMA: " + totalSeconds);
-                        }
-                    }
-                };
-                var timeout = 50;
-                var i = 0;
-                setInterval(function() { 
-                    if ( i >= timeout ) { console.log( "CINEMA: Terminate" ); }
-                    document.querySelector('video').setAttribute('controls', '');
-                    document.querySelector('video').muted = true;
-                    checkSeekBarTime();
-                    i = i + 1;
-                }, 100);
-            ]]
-            pnl:QueueJavascript(METADATA_JS)
-        end
+					var banner = document.querySelector("tiktok-cookie-banner")
+					if (!!banner && !done) {
+						var buttons = banner.shadowRoot.querySelectorAll(".tiktok-cookie-banner .button-wrapper button")
 
-        function panel:ConsoleMessage(msg)
-            if not string.StartsWith(msg, "CINEMA: ") then
-                print(msg)
-                return
-            end
+						if (!!buttons && !!buttons[1]) {
+							var consent = buttons[1]
 
-            local seconds = string.sub(msg, 9, string.len(msg))
-            callback({
-                dur = seconds
-            })
+							consent.click()
+							done = true
+						} else {
+							done = true
+						}
 
-            panel:Remove()
-        end
+						return;
+					}
 
-        panel:OpenURL(data)
-    end
+					clearInterval(checkerInterval)
+
+					player.setAttribute('controls', '')
+					player.muted = false
+
+					window.cinema_controller = player
+					exTheater.controllerReady()
+				}
+			}, 50)
+	]]
+
+	local METADATA_JS = [[
+
+		setTimeout(function(){
+			(async () => {
+				{@Extras} // Added via LUA
+				var videosrc = await document.querySelector(`[href$=\"${contentID}\"]`)
+
+				const response = await fetch(`https://www.tiktok.com/oembed?url=${videosrc.href}`)
+				const json = await response.json()
+
+				var player = document.getElementsByTagName("VIDEO")[0]
+				if (!!player) {
+					var title = (json.title.length == 0 && `@${json.author_name} (${contentID})` || json.title.substr(0, 50)) + " ..."
+					var metadata = {
+						duration: Math.round(player.duration),
+						thumbnail: json.thumbnail_url,
+						title: title
+					}
+					
+					console.log("METADATA:" + JSON.stringify(metadata))	
+				}
+			})()	
+		}, 500)
+	]]
+
+	local BROWSER_JS = [[
+		setInterval(() => {
+			const guestModeContainer = document.querySelector('[class*="DivGuestModeContainer"]');
+			if (guestModeContainer) {
+				console.log('DivGuestModeContainer found:', guestModeContainer);
+
+				const boxContainer = guestModeContainer.querySelector('[class*="DivBoxContainer"]');
+				if (boxContainer) {
+					boxContainer.click();
+					console.log('DivBoxContainer clicked:', boxContainer);
+				} else {
+					console.log('DivBoxContainer not found inside DivGuestModeContainer');
+				}
+			} else {
+				console.log('DivGuestModeContainer not found');
+			}
+		}, 500); 
+	]]
+
+	function SERVICE:LoadProvider(Video, panel)
+		panel:OpenURL(EMBED_URL:format(Video:Data()) ..
+			EMBED_PARAM .. "&autoplay=1"
+		)
+
+		panel.OnDocumentReady = function(pnl)
+			self:LoadExFunctions( pnl )
+			pnl:QueueJavascript(THEATER_JS)
+		end
+	end
+
+	function SERVICE:GetMetadata( data, callback )
+
+		local panel = vgui.Create("DHTML")
+		panel:SetSize(500,500)
+		panel:SetAlpha(0)
+		panel:SetMouseInputEnabled(false)
+
+		function panel:ConsoleMessage(msg)
+
+			if msg:StartWith("METADATA:") then
+				local metadata = util.JSONToTable(string.sub(msg, 10))
+
+				callback(metadata)
+				panel:Remove()
+			end
+
+			if msg:StartWith("ERROR:") then
+				local errmsg = string.sub(msg, 7)
+
+				callback({ err = errmsg })
+				panel:Remove()
+			end
+		end
+
+		panel:OpenURL(EMBED_URL:format(data) ..
+			EMBED_PARAM .. "&autoplay=0"
+		)
+
+		function panel:OnDocumentReady(url)
+			if IsValid(panel) then
+				panel:QueueJavascript(METADATA_JS:Replace("{@Extras}", ([[
+					var contentID = "%s"
+				]]):format(
+						data
+					)
+				))
+			end
+		end
+
+		timer.Simple(10, function()
+			if IsValid(panel) then
+				panel:Remove()
+			end
+		end )
+	end
+
+	function SERVICE:SearchFunctions( browser )
+		if not IsValid( browser ) then return end
+
+		browser:RunJavascript(BROWSER_JS)
+	end
 end
 
 function SERVICE:GetURLInfo(url)
 
-    if url.path then
-        local data = url.path:match("/@[%a%w%d%_%.]+/video/(%d+)$")
-        if data and data ~= nil then
-            return {
-                Data = url.host .. url.path
-            }
-        end
-    end
+	if url.path then
+		local data = url.path:match("/@[%a%w%d%_%.]+/video/(%d+)$")
+		if data then return { Data = data} end
+	end
 
-    return false
+	return false
 end
 
 function SERVICE:GetVideoInfo(data, onSuccess, onFailure)
 
-    theater.FetchVideoMedata(data:GetOwner(), data, function(metadata)
-        if not isnumber(tonumber(metadata.dur)) or tonumber(metadata.dur) < 1 then
-            pcall(onFailure, "Duration Not Found")
-            return
-        end
+	theater.FetchVideoMedata( data:GetOwner(), data, function(metadata)
 
-        http.Fetch(API_URL:format("https://" .. data:Data()), function(body)
+		if metadata.err then
+			return onFailure(metadata.err)
+		end
 
-            local json = util.JSONToTable(body)
-            if json then
-                local info = {
-                    title = "coming soon",
-                    thumbnail = "about:blank",
-                }
+		local info = {}
+		info.title = metadata.title
+		info.duration = tonumber(metadata.duration)
+		info.thumbnail = metadata.thumbnail
 
-                info.title = string.sub(json.title, 1, 42)
-                info.thumbnail = json.thumbnail_url
-                info.duration = tonumber(metadata.dur) + 2
-
-                if onSuccess then
-                    pcall(onSuccess, info)
-                end
-            end
-        end)
-    end)
+		if onSuccess then
+			pcall(onSuccess, info)
+		end
+	end)
 end
 
 theater.RegisterService("tiktok", SERVICE)
