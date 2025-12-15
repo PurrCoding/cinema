5df92349469c33f28f9fcd1b9d5b602764b9257e
Shadowsun™ <12494967+CattoGamer@users.noreply.github.com>
2025-12-15 16:51:42 +0100

    Revert "Optimize draw.HTMLTexture by caching panel dimensions and material"
    This reverts commit e41111cae6f156786460c1861a3d912c49a25851.

--- cinema_modded/gamemode/extensions/cl_draw.lua
+++ cinema_modded/gamemode/extensions/cl_draw.lua
@@ -13,6 +13,7 @@ local FPS_Smoother = CreateClientConVar( "cinema_smoother", 1, true, false, "Mak
 
 local function ChangeFrameCap()
 	local bool = FPS_Smoother:GetBool()
+
 	FPS_Cap = bool and 60 or 30
 end
 ChangeFrameCap()
@@ -25,35 +26,33 @@ function draw.TheaterText(text, font, x, y, colour, xalign, yalign)
 	draw_SimpleText(text, font, x, y, colour, xalign, yalign)
 end
 
+local mat, pw, ph
 function draw.HTMLTexture( panel, w, h )
-	if not (panel and IsValid(panel) and w and h) then return end
+	if not panel or not IsValid(panel) then return end
+	if not w or not h then return end
 
-	-- Initialize cache on first use
-	if not panel._cachedWidth then
-		local pw, ph = panel:GetSize()
-		local w_scalar = w / pw
-		local h_scalar = h / ph
-		local pw_adjusted = pw * (math.power2(pw) / pw)
-		local ph_adjusted = ph * (math.power2(ph) / ph)
-
-		panel._cachedWidth = w_scalar * pw_adjusted
-		panel._cachedHeight = h_scalar * ph_adjusted
-	end
-
-	-- Update texture at throttled rate
 	if not panel.NextHTMLTextureThink or RealTime() > panel.NextHTMLTextureThink then
 		panel:UpdateHTMLTexture()
 		panel.NextHTMLTextureThink = RealTime() + (1 / FPS_Cap)
-		panel._cachedMaterial = panel:GetHTMLMaterial()
 	end
 
-	-- Render using cached values
-	if panel._cachedMaterial then
+	pw, ph = panel:GetSize()
+
+	-- Convert to scalar
+	w = w / pw
+	h = h / ph
+
+	-- Fix for non-power-of-two html panel size
+	pw = pw * (math.power2(pw) / pw)
+	ph = ph * (math.power2(ph) / ph)
+
+	mat = panel:GetHTMLMaterial()
+	if mat then
 		surface_SetDrawColor( 255, 255, 255, 255 )
-		surface_SetMaterial( panel._cachedMaterial )
-		surface_DrawTexturedRect( 0, 0, panel._cachedWidth, panel._cachedHeight )
+		surface_SetMaterial( mat )
+		surface_DrawTexturedRect( 0, 0, w * pw, h * ph )
 	else
 		surface_SetDrawColor( 0, 0, 0, 255 )
-		surface_DrawRect( 0, 0, panel._cachedWidth, panel._cachedHeight )
+		surface_DrawRect( 0, 0, w * pw, h * ph )
 	end
 end
